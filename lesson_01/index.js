52ff214515d5729ac27b818a76cad50cfa7d8058
Максим Сараев <max.saraev.web@gmail.com>
2023-06-28 17:20:29 +0300

    initial

---
+++ lesson_01/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const product = 'AMD FX-8320';
+const count = 2;
+const category = 'CPU';
+const price = 250;
+
+console.log('product: ', product);
+
+console.log('total_price: ', price * count);
