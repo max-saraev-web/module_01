c442b05e883153b3460c63e40655dfd5a6cbb9b1
Максим Сараев <max.saraev.web@gmail.com>
2023-06-29 15:46:18 +0300

    lesson_03: hw

---
+++ lesson_03/einstein.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const myWeight = 77,
+    speedLight = 3e8;
+
+console.log(`
+    Энергия моего тела равна: ${myWeight * Math.pow(speedLight, 2)}
+`);
---
+++ lesson_03/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+{
+    const item = 'AMD FX-8320';
+    const count = 2;
+    const category = 'CPU';
+    const price = 250;
+
+    console.log('product: ', item);
+
+    console.log('total_price: ', price * count);
+}
+
+{
+    const item = 'Radeon 7900XTX';
+    const count = 3;
+    const category = 'GPU';
+    const price = 1250;
+}
+
+{
+    const item = prompt('Наименование товара');
+    const count = prompt('Количество товара');
+    const category = prompt('Категория товара');
+    const price = prompt('Цена товара');
+
+    console.log(`
+        На складе ${count} единицы товара "${item}" на сумму ${price * count} деревянных
+    `);
+}
