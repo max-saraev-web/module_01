cfd098c5f365e92ce0cb66cd40cee0faedde70a3
Максим Сараев <max.saraev.web@gmail.com>
2023-06-30 16:55:00 +0300

    task1: complete

---
+++ lesson_04/index.js
@@ -0,0 +1,36 @@
+'use strict';
+/*
+{
+    const item = 'AMD FX-8320';
+    const count = 2;
+    const category = 'CPU';
+    const price = 250;
+
+    console.log('product: ', item);
+
+    console.log('total_price: ', price * count);
+}
+
+{
+    const item = 'Radeon 7900XTX';
+    const count = 3;
+    const category = 'GPU';
+    const price = 1250;
+}
+*/
+
+{
+    const item = prompt('Наименование товара');
+    const count = prompt('Количество товара');
+    const category = prompt('Категория товара');
+    const price = prompt('Цена товара');
+    const total = price * count;
+
+    if(total > 0){
+        console.log(`
+            На складе ${count} единицы товара "${item}" на сумму ${total} деревянных
+        `);
+    }else{
+        console.warn('Вы ввели некорректные данные');
+    }
+}
