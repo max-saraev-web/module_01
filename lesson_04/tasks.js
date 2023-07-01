ff145e49b6e6ba0f422db9cae7eb373e949f4422
Максим Сараев <max.saraev.web@gmail.com>
2023-07-01 16:03:56 +0300

    task3: done

--- lesson_04/tasks.js
+++ lesson_04/tasks.js
@@ -1,9 +1,27 @@
 'use strcit';
 
-const rain = Math.round(Math.random());
+// ! - Задача 2
+/*
+{const rain = Math.round(Math.random());
 
 if(rain === 1){
     console.log('Возьмите зонт');
 }else{
     console.log('Дождя нет!');
+}}
+*/
+
+// ! - Задача 3
+{
+    let total = 0;
+
+    total += +prompt('Введите кол-во баллов по математике: ');
+    total += +prompt('Введите кол-во баллов по русскому языку: ');
+    total += +prompt('Введите кол-во баллов по информатике ');
+
+    if(total >= 265 && total < 300){
+        console.log('Поздравляю, вы поступили на бюджет!');
+    }else{
+        console.log('Вы не готовы!');
+    }
 }
