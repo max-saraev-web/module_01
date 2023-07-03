6a50b292d5ddbcbefba1ec214057797792d84f75
Максим Сараев <max.saraev.web@gmail.com>
2023-07-03 17:41:06 +0300

    raw draft of taskHard_2

--- lesson_04/tasksHard_2.js
+++ lesson_04/tasksHard_2.js
@@ -20,55 +20,69 @@
 
 // * - Решение
 {
-    const income = +prompt('Введите ваш месячный доход.');
+    let income = +prompt('Введите ваш месячный доход.');
 
     let tax = 0;
-    let extraIncome = 0;
-    let taxableIncome = 0;
+    /* let extraIncome = 0;
+    let taxableIncome = 0; */
 
-    if(income <= 15000){
+    // if(income <= 15000){
         
-        let deduction = 0.13;
-        tax = taxCalc(income, deduction);
+    //     let deduction = 0.13;
+    //     tax = taxCalc(income, deduction);
 
-        console.log(`
-            Уважаемый клиент, за этот месяц вы заработали ${income}, сумма вашего налога    составляет ${tax}, а в процентах это ${deduction * 100}%. 
+    //     console.log(`
+    //         Уважаемый клиент, за этот месяц вы заработали ${income}, сумма вашего налога    составляет ${tax}, а в процентах это ${deduction * 100}%. 
 
-            Хорошего вам дня!
-        `);
-    } else if(income > 15000 && income <= 50000){
+    //         Хорошего вам дня!
+    //     `);
+    // } else if(income > 15000 && income <= 50000){
 
-        let deduction = 0.13;
-        tax = taxCalc(income, deduction);
+    //     let deduction = 0.13;
+    //     tax = taxCalc(income, deduction);
 
-        let extraDeduction = 0.2;
-        extraIncome = income - 15000;
+    //     let extraDeduction = 0.2;
+    //     extraIncome = income - 15000;
 
-        taxableIncome = taxCalc(extraIncome, extraDeduction);
+    //     taxableIncome = taxCalc(extraIncome, extraDeduction);
 
-        console.log(`
-            Уважаемый клиент, за этот месяц вы заработали ${income}, сумма вашего налога  составляет ${tax + taxableIncome}. 
+    //     console.log(`
+    //         Уважаемый клиент, за этот месяц вы заработали ${income}, сумма вашего налога  составляет ${tax + taxableIncome}. 
 
-            Хорошего вам дня!
-        `);
-    } else if(income > 50000){
+    //         Хорошего вам дня!
+    //     `);
+    // } else if(income > 50000){
         
-        let deduction = 0.13;
-        tax = taxCalc(income, deduction);
+    //     let deduction = 0.13;
+    //     tax = taxCalc(income, deduction);
 
-        let extraDeduction = 0.3;
-        extraIncome = income - 15000;
+    //     let extraDeduction = 0.3;
+    //     extraIncome = income - 15000;
 
-        taxableIncome = taxCalc(extraIncome, extraDeduction);
+    //     taxableIncome = taxCalc(extraIncome, extraDeduction);
 
-        console.log(`
-            Уважаемый клиент, за этот месяц вы заработали ${income}, сумма вашего налога  составляет ${tax + taxableIncome}. 
+    //     console.log(`
+    //         Уважаемый клиент, за этот месяц вы заработали ${income}, сумма вашего налога  составляет ${tax + taxableIncome}. 
 
-            Хорошего вам дня!
-        `);
+    //         Хорошего вам дня!
+    //     `);
+    // }
+    if(income > 50000){
+        tax += (income - 50000) * 0.3;
+        income -=income -50000;
     }
-
-    function taxCalc(income, percent) {   
-        return income * percent;
+    if(income > 15000){
+        tax += (income - 15000) * 0.2;
+        income -=income -15000;
     }
+    if(income < 15000 && income > 0){
+        tax += income * 0.13;
+    }
+    console.log(`
+        Ваш налог составляет ${tax}
+    `);
+
+    // function taxCalc(income, percent) {   
+    //     return income * percent;
+    // }
 }
