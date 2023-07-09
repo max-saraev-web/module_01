becfc446961439409fc98e7b124d6a78ffa05f81
Максим Сараев <max.saraev.web@gmail.com>
2023-07-09 15:59:14 +0300

    task04 - done

---
+++ lesson_08/task04.js
@@ -0,0 +1,23 @@
+'use strict';
+// ! - формула нахождения весокостного года 
+
+
+const generator = (n, m) => {
+
+    const newArr = [];
+
+    for (let i = n; i < m; i++) {
+        newArr.push(Math.round(Math.random() * (m - n) + n));        
+    }
+
+    let sortedArr = [];
+
+    sortedArr = newArr.filter((n) => {
+        if(n % 4 === 0 && (n % 100 !== 0 || n % 400 === 0)){
+            return n;
+        }
+    });
+    return sortedArr;
+};
+
+console.log(generator(1764, 2024));
