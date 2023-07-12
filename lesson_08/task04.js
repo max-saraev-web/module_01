b174d04a4617106bf9dfd62e2fa4d3a541d1b3ed
Максим Сараев <max.saraev.web@gmail.com>
2023-07-12 17:41:15 +0300

    changes

--- lesson_08/task04.js
+++ lesson_08/task04.js
@@ -1,22 +1,42 @@
 'use strict'; 
-
+// ! - условие нахождение високосного года 
+// n % 4 === 0 && (n % 100 !== 0 || n % 400 === 0
 
 const generator = (n, m) => {
 
-    const newArr = [];
+    const functions = {
+        getRange(min, max){
+            if(min === max){
+                return 1;
+            }
+            return 1 + getRange(min + 1, max);
+        },
+        isLeap(num){
+            return num % 4 === 0 && (num % 100 !== 0 || num % 400 === 0) ? true : false;
+        },
+    };
 
-    for (let i = n; i < m; i++) {
-        newArr.push(Math.round(Math.random() * (m - n) + n));        
+    const{getRange, isLeap} = functions;
+
+    const range = getRange(n, m);
+
+    const arr = [];
+
+    const getYear = (range) => {
+        let step = range;
+        if(step === 0){
+            return;
+        }else if ((isLeap(step))){
+            arr.push(step);
+            return getYear(step - 1);
+        }else{
+            return getYear(step - 1);
+        }
     }
 
-    let sortedArr = [];
 
-    sortedArr = newArr.filter((n) => {
-        if(n % 4 === 0 && (n % 100 !== 0 || n % 400 === 0)){
-            return n;
-        }
-    });
-    return sortedArr;
+    getYear(range)
+    return arr;
 };
 
 console.log(generator(1764, 2024));
