04bf719e0f35aa6d487affda984757ce4390238e
Максим Сараев <max.saraev.web@gmail.com>
2023-07-18 19:02:10 +0300

    done

---
+++ lesson_10/cart.js
@@ -0,0 +1,56 @@
+'use strict';
+
+import {listIPv4 as ip} from './ipv4.js';
+
+const cart = {
+    items: [],
+    // total: 0,
+    count: 0,
+    get totalPrice(){
+        return `Общаяя стоймость товаров ${this.calculateItemPrice()}`;
+    },
+    calculateItemPrice(){
+        return this.items.reduce((acc, current) => {
+            return acc + current.itemPrice;
+        }, 0);
+    },
+    add(itemName, itemPrice, itemCount = 1){
+        const newItem = {
+            itemName: itemName,
+            itemPrice: itemPrice,
+            itemCount: itemCount,
+        };
+        this.items.push(newItem);
+        this.count = this.items.reduce((acc, current) => {
+            return acc + current.itemCount;
+        }, 0);
+        
+    },
+    increaseCount(number){
+        this.count += number;
+    },
+    clear(){
+        this.items = [];
+        this.count = 0;
+    },
+    print(){
+        console.log(
+            JSON.stringify(this.items),
+            this.totalPrice
+        );
+    },
+};
+
+cart.add('сок добрый - апельсин', 35);
+cart.add('сок добрый - манго', 35);
+cart.add('сок добрый - томат', 35);
+
+// cart.print();
+
+// ! - Задание 2: вывод количества уникальных ip-адресов
+
+
+const setLenght = ([...arr]) =>{ 
+    return `Количество уникальных ip-адресов: ${new Set(arr).size}`;
+};
+console.log(setLenght(ip));
