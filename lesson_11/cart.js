13dc313b6ebc933fa69bf1c179fe40af34f1c362
max-saraev-web <max.saraev.web@gmail.com>
2023-07-22 13:04:33 +0300

    eslint-fix

--- lesson_11/cart.js
+++ lesson_11/cart.js
@@ -1,44 +1,41 @@
-'use strict';
+
 
 import {listIPv4 as ip} from './ipv4.js';
 
 const cart = {
-    items: [],
-    // total: 0,
-    count: 0,
-    get totalPrice(){
-        return `Общаяя стоймость товаров ${this.calculateItemPrice()}`;
-    },
-    calculateItemPrice(){
-        return this.items.reduce((acc, current) => {
-            return acc + current.itemPrice;
-        }, 0);
-    },
-    add(itemName, itemPrice, itemCount = 1){
-        const newItem = {
-            itemName: itemName,
-            itemPrice: itemPrice,
-            itemCount: itemCount,
-        };
-        this.items.push(newItem);
-        this.count = this.items.reduce((acc, current) => {
-            return acc + current.itemCount;
-        }, 0);
-        
-    },
-    increaseCount(number){
-        this.count += number;
-    },
-    clear(){
-        this.items = [];
-        this.count = 0;
-    },
-    print(){
-        console.log(
-            JSON.stringify(this.items),
-            this.totalPrice
-        );
-    },
+  items: [],
+  // total: 0,
+  count: 0,
+  get totalPrice() {
+    return `Общаяя стоймость товаров ${this.calculateItemPrice()}`;
+  },
+  calculateItemPrice() {
+    return this.items
+      .reduce((acc, current) => acc + current.itemPrice, 0);
+  },
+  add(itemName, itemPrice, itemCount = 1) {
+    const newItem = {
+      itemName,
+      itemPrice,
+      itemCount,
+    };
+    this.items.push(newItem);
+    this.count = this.items
+      .reduce((acc, current) => acc + current.itemCount, 0);
+  },
+  increaseCount(number) {
+    this.count += number;
+  },
+  clear() {
+    this.items = [];
+    this.count = 0;
+  },
+  print() {
+    console.log(
+      JSON.stringify(this.items),
+      this.totalPrice,
+    );
+  },
 };
 
 cart.add('сок добрый - апельсин', 35);
@@ -50,7 +47,6 @@ cart.add('сок добрый - томат', 35);
 // ! - Задание 2: вывод количества уникальных ip-адресов
 
 
-const setLenght = ([...arr]) =>{ 
-    return `Количество уникальных ip-адресов: ${new Set(arr).size}`;
-};
+const setLenght = ([...arr]) => `
+  Количество уникальных ip-адресов: ${new Set(arr).size}`;
 console.log(setLenght(ip));
