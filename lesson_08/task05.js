eec2e2df98e7433675a0e86cc2e4c7754a98f78d
max-saraev-web <max.saraev.web@gmail.com>
2023-08-01 15:55:59 +0300

    task05: refact to the new code style

--- lesson_08/task05.js
+++ lesson_08/task05.js
@@ -3,59 +3,59 @@
 const botNum = Math.round((Math.random() * 99) + 1);
 console.log('botNum: ', botNum);
 
-const numberBot = (bot, user) =>{
-    document.documentElement.innerHTML = '';
-    
-    if (user === null) {
-        alert('Вы вышли из игры! До свидания!');
+const numberBot = (bot, user) => {
+  document.documentElement.innerHTML = '';
 
-        document.documentElement.innerHTML = `
+  if (user === null) {
+    alert('Вы вышли из игры! До свидания!');
+
+    document.documentElement.innerHTML = `
         <h1 class='victory'>
             Увы, но вы отказались от игры :(
         </h1>
         `;
-        const title = document.querySelector('.victory');
-        title.style.cssText = `
+    const title = document.querySelector('.victory');
+    title.style.cssText = `
             text-align: center;
             text-align: center;
             color: red;
         `;
-        return null;
-    }
+    return null;
+  }
 
-    let userNum = parseInt(user);
+  const userNum = parseInt(user);
 
 
-    if(isNaN(userNum)){
-        alert('Введите число!');
-        return numberBot(bot, prompt('Введите ваш вариант отгадки!'));
-    }
+  if (isNaN(userNum)) {
+    alert('Введите число!');
+    return numberBot(bot, prompt('Введите ваш вариант отгадки!'));
+  }
 
-    if(userNum > botNum){
-        alert('Меньше!');
-        return numberBot(bot, prompt('Введите ваш вариант отгадки!'));
-    }
-    if(userNum < botNum){
-        alert('Больше!');
-        return numberBot(bot, prompt('Введите ваш вариант отгадки!'));
-    }
+  if (userNum > botNum) {
+    alert('Меньше!');
+    return numberBot(bot, prompt('Введите ваш вариант отгадки!'));
+  }
+  if (userNum < botNum) {
+    alert('Больше!');
+    return numberBot(bot, prompt('Введите ваш вариант отгадки!'));
+  }
 
-    if(botNum === userNum){
-        alert('Правильно!');
-        document.documentElement.innerHTML = `
+  if (botNum === userNum) {
+    alert('Правильно!');
+    document.documentElement.innerHTML = `
             <h1 class='victory'>
                 Игра законченна - вы победили!
             </h1>
         `;
-        const title = document.querySelector('.victory');
-        title.style.cssText = `
+    const title = document.querySelector('.victory');
+    title.style.cssText = `
             text-align: center;
             text-align: center;
             color: red;
         `;
-        return;
-    }
+    return;
+  }
 };
-let userNum = prompt('Введите ваш вариант отгадки!');
+const userNum = prompt('Введите ваш вариант отгадки!');
 
 numberBot(botNum, userNum);
